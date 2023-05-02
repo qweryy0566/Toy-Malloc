b865324a9c8a6e4bbd28634d58e5151a9b98a06a
qweryy0566 <qweryy0566@qq.com>
2023-05-02 12:07:22 +0800

    opt: remove the boundary tags of allocated blocks

--- mm.c
+++ mm.c
@@ -35,8 +35,9 @@
 #define WSIZE 4
 #define ALIGNMENT 8
 #define DSIZE 8
-#define CHUNKSIZE (1 << 9)
+#define CHUNKSIZE 456
 #define MIN_BLK_SIZE 24
+#define MAX_HEAP 0xffffffff 
 
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
@@ -47,14 +48,15 @@ static char *heap_listp = 0;
 // static int cnt = 0;
 
 /* read or write a word at address p */
-#define GET(p) ((p) ? *(unsigned int *)(p) : 0)
+#define GET(p) ((p) ? *(unsigned *)(p) : 0)
 #define GET_PTR(p) ((p) ? (void *)*(size_t *)(p) : 0)
-#define PUT(p, val) ((p) ? *(unsigned int *)(p) = (val) : 0)
+#define PUT(p, val) ((p) ? *(unsigned *)(p) = (val) : 0)
 #define PUT_PTR(p, ptr) ((p) ? *(size_t *)(p) = (size_t)(ptr) : 0)
 
 /* read the size and allocated fields from address p */
 #define READ_SIZE(p) (GET(p) & ~0x7)
 #define READ_ALLOC(p) (GET(p) & 0x1)
+#define READ_L_ALLOC(p) (GET(p) & 0x2)
 
 /* given block ptr bp, compute address of its header, footer, prev, and next */
 #define HEAD(bp) ((char *)(bp) - WSIZE)
@@ -75,22 +77,22 @@ static char *heap_listp = 0;
  */
 int mm_init(void) {
   if ((heap_listp = mem_sbrk(8 * WSIZE)) == (void *)-1) return -1;
-  PUT(heap_listp + 1 * WSIZE, PACK(6 * WSIZE, 1)); /* prologue header */
+  PUT(heap_listp + 1 * WSIZE, PACK(6 * WSIZE, 3)); /* prologue header */
   PUT_PTR(heap_listp + 2 * WSIZE, 0);              /* prev pointer */
   PUT_PTR(heap_listp + 4 * WSIZE, 0);              /* next pointer */
-  PUT(heap_listp + 6 * WSIZE, PACK(6 * WSIZE, 1)); /* prologue footer */
-  PUT(heap_listp + 7 * WSIZE, PACK(0, 1));         /* epilogue header */
+  PUT(heap_listp + 6 * WSIZE, PACK(6 * WSIZE, 3)); /* prologue footer */
+  PUT(heap_listp + 7 * WSIZE, PACK(0, 3));         /* epilogue header */
   heap_listp += 2 * WSIZE;
   dbg_printf("heap_size = %ld\n", mem_heapsize());
   return 0;
 }
 
-static void delete_from_list(void *bp) {
+static inline void delete_from_list(void *bp) {
   dbg_printf("delete_from_list, pre = %p\n", PRE_BLK_AT(bp));
   PUT_PTR(NXT_BLK_F(PRE_BLK_AT(bp)), NXT_BLK_AT(bp));
   PUT_PTR(PRE_BLK_F(NXT_BLK_AT(bp)), PRE_BLK_AT(bp));
 }
-static void add_to_list(void *bp) {
+static inline void add_to_list(void *bp) {
   void *last_blk = NXT_BLK_AT(heap_listp);
   PUT_PTR(PRE_BLK_F(last_blk), bp);
   PUT_PTR(NXT_BLK_F(bp), last_blk);
@@ -98,23 +100,28 @@ static void add_to_list(void *bp) {
   PUT_PTR(NXT_BLK_F(heap_listp), bp);
 }
 
+static inline void change_alloc(void *bp, size_t size, unsigned alloc) {
+  PUT(HEAD(bp), PACK(size, READ_L_ALLOC(HEAD(bp)) | alloc));
+  if (!alloc) PUT(FOOT(bp), PACK(size, READ_L_ALLOC(HEAD(bp)) | alloc));
+  PUT(HEAD(R_BLK(bp)), PACK(READ_SIZE(HEAD(R_BLK(bp))),
+                            alloc << 1 | READ_ALLOC(HEAD(R_BLK(bp)))));
+}
+
 static void *coalesce_and_add(void *bp) {
   dbg_printf("coalesce_and_add, bp = %p\n", bp);
   size_t size = READ_SIZE(HEAD(bp));
-  if (!READ_ALLOC(FOOT(L_BLK(bp)))) {
+  if (!READ_L_ALLOC(HEAD(bp))) {
     dbg_printf("coalesce left, bp = %p\n", L_BLK(bp));
     size += READ_SIZE(FOOT(L_BLK(bp)));
     delete_from_list(L_BLK(bp));
     bp = L_BLK(bp);
-    PUT(HEAD(bp), PACK(size, 0));
-    PUT(FOOT(bp), PACK(size, 0));
+    change_alloc(bp, size, 0);
   }
   if (!READ_ALLOC(HEAD(R_BLK(bp)))) {
     dbg_printf("coalesce right, bp = %p\n", R_BLK(bp));
     size += READ_SIZE(HEAD(R_BLK(bp)));
     delete_from_list(R_BLK(bp));
-    PUT(HEAD(bp), PACK(size, 0));
-    PUT(FOOT(bp), PACK(size, 0));
+    change_alloc(bp, size, 0);
   }
   dbg_printf("coalesce size = %ld, bp = %p\n", size, bp);
   add_to_list(bp);
@@ -123,17 +130,15 @@ static void *coalesce_and_add(void *bp) {
 
 /*
  * place - Place block of size bytes at start of block bp (MAYBE NOT FREE)
- *         and split if remainder would be at least minimum block size
+ *     and split if remainder would be at least minimum block size.
  */
 static void place(void *bp, size_t size) {
   size_t blk_size = READ_SIZE(HEAD(bp));
-  unsigned int is_alloc = READ_ALLOC(HEAD(bp));
+  unsigned is_alloc = READ_ALLOC(HEAD(bp));
   dbg_printf("place, is_alloc = %u\n", READ_ALLOC(HEAD(bp)));
   if (blk_size >= size + MIN_BLK_SIZE) { /* split */
-    PUT(HEAD(bp), PACK(size, 1));
-    PUT(FOOT(bp), PACK(size, 1));
-    PUT(HEAD(R_BLK(bp)), PACK(blk_size - size, 0));
-    PUT(FOOT(R_BLK(bp)), PACK(blk_size - size, 0));
+    change_alloc(bp, size, 1);
+    change_alloc(R_BLK(bp), blk_size - size, 0);
     if (is_alloc) add_to_list(R_BLK(bp));
     else {
       PUT_PTR(NXT_BLK_F(PRE_BLK_AT(bp)), R_BLK(bp));
@@ -143,21 +148,20 @@ static void place(void *bp, size_t size) {
     }
   } else { /* no split */
     if (!is_alloc) delete_from_list(bp);
-    PUT(HEAD(bp), PACK(blk_size, 1));
-    PUT(FOOT(bp), PACK(blk_size, 1));
+    change_alloc(bp, blk_size, 1);
   }
 }
 
 /*
- * extend_heap - 
- *
+ * extend_heap - Extend heap by calling mem_sbrk and return the new block bp.
+ *      Also add the new block to the free list and coalesce if necessary.
  */
 static void *extend_heap(size_t size) {
   void *bp = mem_sbrk(size);
   if (bp == (void *)-1) return NULL;
-  PUT(HEAD(bp), PACK(size, 0));
-  PUT(FOOT(bp), PACK(size, 0));
-  PUT(HEAD(R_BLK(bp)), PACK(0, 1)); /* new epilogue header */
+  change_alloc(bp, size, 0);
+  /* new epilogue header */
+  PUT(HEAD(R_BLK(bp)), PACK(0, READ_L_ALLOC(HEAD(R_BLK(bp))) | 1));
   return coalesce_and_add(bp);
 }
 
@@ -168,7 +172,7 @@ static void *extend_heap(size_t size) {
 void *malloc(size_t size) {
   // if (size == 0) return NULL;
   // ++cnt; dbg_printf("#%d malloc %ld\n", cnt, size);
-  size = ALIGN(MAX(MIN_BLK_SIZE, size + 2 * WSIZE));
+  size = ALIGN(MAX(MIN_BLK_SIZE, size + WSIZE));
   void *bp = NXT_BLK_AT(heap_listp);
   dbg_printf("bp = %p\n", bp);
   for (; bp; bp = NXT_BLK_AT(bp)) /* explicit free list */
@@ -193,8 +197,7 @@ void free(void *ptr) {
   // ++cnt; dbg_printf("#%d free %p\n", cnt, ptr);
   if (ptr < mem_heap_lo() || ptr > mem_heap_hi()) return;
   size_t size = READ_SIZE(HEAD(ptr));
-  PUT(HEAD(ptr), PACK(size, 0));
-  PUT(FOOT(ptr), PACK(size, 0));
+  change_alloc(ptr, size, 0);
   coalesce_and_add(ptr);
 }
 
@@ -219,7 +222,7 @@ void *realloc(void *oldptr, size_t size) {
     return malloc(size);
   }
   oldsize = READ_SIZE(HEAD(oldptr));
-  size = ALIGN(MAX(MIN_BLK_SIZE, size + 2 * WSIZE));
+  size = ALIGN(MAX(MIN_BLK_SIZE, size + WSIZE));
   if (size <= oldsize) {
     place(oldptr, size);
     newptr = oldptr;
@@ -227,8 +230,7 @@ void *realloc(void *oldptr, size_t size) {
              oldsize + READ_SIZE(HEAD(R_BLK(oldptr))) >= size) {
     delete_from_list(R_BLK(oldptr));
     size_t right_size = READ_SIZE(HEAD(R_BLK(oldptr)));
-    PUT(HEAD(oldptr), PACK(oldsize + right_size, 1));
-    PUT(FOOT(oldptr), PACK(oldsize + right_size, 1));
+    change_alloc(oldptr, oldsize + right_size, 1);
     place(oldptr, size);
     newptr = oldptr;
   } else {
